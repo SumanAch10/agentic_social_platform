503a1edd998fe56c07ec9ea7dddd79f544472d13
SumanAch10 <contact.sumanacharya@gmail.com>
2025-06-30 22:46:43 -0500

    Creating request

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,34 +1,20 @@
 from app.schemas import users
 from fastapi import FastAPI,HTTPException,status,APIRouter
 from app.db import db
+from app.models import models
 
 router = APIRouter()
 
-@router.post("/create_user")
-def create_user(user:users.signUp_user):
-    
-    if(user.confirm_password == user.password):
-        for u in db.signUp_user:
-            if(u["email"] == user.email):
-                raise HTTPException(status_code = 409,detail= "Email already registered")
-        db.signUp_user.append(user.dict())
-        return {"message":"User registered successfully","User":db.signUp_user}
-    
-    raise HTTPException(status_code = 400,detail="Password donot match")
+# Signing a new user
+@router.post("/user_create")
+def create_user():
+    pass
 
-@router.post("/login_user")
-def signIn_user(user:users.signIn_user):
-    print("Sign in hit!!")
-    for u in db.signUp_user:
-        print("Inside the loop")
-        print("Checking the if else")
-        if(u["email"] == user.email and u["password"] == user.password):
-            print("Entering into the if else")
-            db.signIn_user.append(user.dict())
-            return {"message":"User authenticated succesfully"}  
-        
-        else:
-            return {"message":"Email or password is invalid!!!!"}
+# Loggin in an user
+@router.post("user_login")
+def login_user():
+    pass
+
             
     
         
