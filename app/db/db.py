e0e6cd5679a628c4ee231a66fbd5ef916235fdaf
SumanAch10 <contact.sumanacharya@gmail.com>
2025-06-30 22:45:47 -0500

    Creating database connector

--- app/db/db.py
+++ app/db/db.py
@@ -1,3 +1,13 @@
-signIn_user = []
-signUp_user = []
+# This is the database connector
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+DATABASE_URL = "postgresql://postgres:<your_password>@localhost:5432/Agentic_social_platform"
+
+# Create Engine
+engine = create_engine(DATABASE_URL)
+
+#Create a session factory
+SessionLocal = sessionmaker(autocommit = False,autoflush=False,bind=engine) 
+
+Base = declarative_base()
