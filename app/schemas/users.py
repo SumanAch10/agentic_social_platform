7e16079bc04e8c6ab06ca8a3c34eed113cd47e1b
SumanAch10 <contact.sumanacharya@gmail.com>
2025-06-30 22:47:10 -0500

    Building users schemas

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,12 +1,13 @@
 # Defining the schemas for my users in this file
 from pydantic import BaseModel,EmailStr
 
-class signIn_user(BaseModel):
-    email:EmailStr
-    password:str
-    
-class signUp_user(BaseModel):
+class UserCreate(BaseModel):
+    user_name : str
     email:EmailStr
     password:str
     confirm_password:str
+    
+class UserLogin(BaseModel):
+    email:EmailStr
+    password:str
 
