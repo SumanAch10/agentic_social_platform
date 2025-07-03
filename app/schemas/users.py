d40c8d888fd4368d19ccd805041fdb662708ee6c
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-02 21:24:39 -0500

    User schema modified

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,13 +1,13 @@
 # Defining the schemas for my users in this file
-from pydantic import BaseModel,EmailStr
+from pydantic import BaseModel,EmailStr,constr
 
 class UserCreate(BaseModel):
     user_name : str
     email:EmailStr
-    password:str
+    password:constr(min_length=6)
     confirm_password:str
     
 class UserLogin(BaseModel):
     email:EmailStr
-    password:str
+    password:constr(min_length=6)
 
