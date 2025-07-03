a03caf910a7dfbefb1a5168bdf14239307a1f614
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-03 17:44:11 -0500

    API endpoints

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,21 +1,23 @@
-# from app.schemas import users
-from fastapi import FastAPI,HTTPException,status,APIRouter
-# from app.db import db
-# from app.models import models
+from app.schemas import users
+from sqlalchemy.orm import Session
+from fastapi import FastAPI,HTTPException,status,APIRouter,Depends
+from app.db import db
+from app.schemas.users import UserCreate,UserLogin
+from app.models import models
+from app.services import services
 
 
 router = APIRouter()
-print(type(router))
+# print(type(router))
 
-# Signing a new user
 @router.post("/user_create")
-def create_user():
-    pass
+def create_user_route(user: UserCreate):
+    return services.create_user(user)
 
 # Loggin in an user
-@router.post("user_login")
-def login_user():
-    pass
+@router.post("/user_login")
+def login_user(user:UserLogin):
+    return services.login_user(user)
 
             
     
