483b4e004e9f56ed097d54cd09def1c01ae9a1f9
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-06 21:18:02 -0500

    Utilities function

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,6 +1,8 @@
 from passlib.context import CryptContext
 from datetime import datetime,timedelta
 from jose import jwt, JWTError
+from fastapi import Depends,HTTPException,status
+from fastapi.security import OAuth2PasswordBearer
 
 # Configuration variable
 SECRET_KEY = "your_secret_key"
@@ -8,6 +10,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="user_login")
 
 def hash_password(password: str):
     return pwd_context.hash(password)
@@ -25,3 +28,26 @@ def create_access_token(data:dict):
     to_encode.update({"exp":expire})
     print("After updating the to_encode variable: ",to_encode)
     return jwt.encode(to_encode,SECRET_KEY,algorithm = ALGORITHM)
+
+def verify_token(token: str = Depends(oauth2_scheme)):
+    try:
+        # Decode the token using the secret and algorithm
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")  # 'sub' stores subject — in our case, email
+
+        # If email doesn't exist in token, token is invalid
+        if email is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+            )
+
+        return email  # You can return this to your route function
+
+    except JWTError:
+        # If token is tampered or expired
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+        )
+
