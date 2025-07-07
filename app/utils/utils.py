04c0e2941393bb10b81c7ec8c1a9bc573bb88dee
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-06 20:26:29 -0500

    Create access token util func added

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,4 +1,11 @@
 from passlib.context import CryptContext
+from datetime import datetime,timedelta
+from jose import jwt, JWTError
+
+# Configuration variable
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +14,14 @@ def hash_password(password: str):
 
 def verify_password(plain_password:str,hash_password:str)->bool:
     return pwd_context.verify(plain_password,hash_password)
+
+def create_access_token(data:dict):
+    to_encode = data.copy()
+    print("To encode variable value: ",to_encode)
+    print(type(to_encode))
+    print("UTC now func: ",datetime.utcnow())
+    
+    expire = datetime.utcnow() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp":expire})
+    print("After updating the to_encode variable: ",to_encode)
+    return jwt.encode(to_encode,SECRET_KEY,algorithm = ALGORITHM)
