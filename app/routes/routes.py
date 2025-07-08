fd9312e81bd960684237213d391b639536ca0318
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-07 21:04:55 -0500

    Routing using jwt tokens

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,7 +1,7 @@
 from app.services import services
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
 from app.schemas.users import UserCreate,UserLogin
-
+from app.utils.utils  import verify_token
 
 router = APIRouter()
 # print(type(router))
@@ -15,13 +15,15 @@ def create_user_route(user: UserCreate):
 def login_user(user:UserLogin):
     return services.login_user(user)
 
+@router.get("/protected")
+def get_protected_route(current_user_email: str = Depends(verify_token)):
+    return {"message": f"Welcome {current_user_email}"}
+
 @router.get("/{user_name}")
 def get_byuser_name(user_name:str):
     return services.getUser(user_name)
 
-@router.get("/protected")
-def get_protected_route(current_user_email: str = Depends(verify_token)):
-    return {"message": f"Welcome {current_user_email}"}
+
 
             
     
