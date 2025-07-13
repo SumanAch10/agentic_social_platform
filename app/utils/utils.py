9c3c10d6b8cbd352a266ce967992db2c7b171f80
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-13 09:52:55 -0500

    Utilities functions added

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -11,44 +11,25 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="user_login")
-print("O auth scheme: ",oauth2_scheme)
+print("O auth scheme type: ",type(oauth2_scheme))
 
+# print(oauth2_scheme)
 def hash_password(password: str):
     return pwd_context.hash(password)
 
 def verify_password(plain_password:str,hash_password:str)->bool:
     return pwd_context.verify(plain_password,hash_password)
 
-def create_access_token(data:dict):
-    to_encode = data.copy()
-    print("To encode variable value: ",to_encode)
-    print(type(to_encode))
-    print("UTC now func: ",datetime.utcnow())
+def create_access_token(token_payload:dict):
+    expiry_time = datetime.utcnow() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
     
-    expire = datetime.utcnow() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp":expire})
-    print("After updating the to_encode variable: ",to_encode)
-    return jwt.encode(to_encode,SECRET_KEY,algorithm = ALGORITHM)
+    # Adding the expiry_time to the payload
+    token_payload.update({"exp":expiry_time})
+    jwt_token = jwt.encode(claims = token_payload,key = SECRET_KEY,algorithm = ALGORITHM)
+    return jwt_token
 
-def verify_token(token: str = Depends(oauth2_scheme)):
-    try:
-        # Decode the token using the secret and algorithm
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email: str = payload.get("sub")  # 'sub' stores subject — in our case, email
+# Verigying the token and then creating a db session
+def verify_access_token():
+    pass
 
-        # If email doesn't exist in token, token is invalid
-        if email is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Could not validate credentials",
-            )
-
-        return email  # You can return this to your route function
-
-    except JWTError:
-        # If token is tampered or expired
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or expired token",
-        )
 
