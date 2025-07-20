fab0432c4b86850215fa1009eb6d9aa72026e31d
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-20 16:45:13 -0500

    Nothing changed much

--- app/db/db.py
+++ app/db/db.py
@@ -1,11 +1,13 @@
 # This is the database connector
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from app.models import models
 
 DATABASE_URL = "postgresql://postgres:1415@localhost:5432/Agentic_social_platform"
 
 # Create Engine
 engine = create_engine(DATABASE_URL)
+print("Hello world")
 
 #Create a session factory
 SessionLocal = sessionmaker(autocommit = False,autoflush=False,bind=engine) 
