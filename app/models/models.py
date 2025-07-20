ef817c57c337079c6673ce904b5304f2f4117bd8
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-20 16:45:29 -0500

    Models for refresh token

--- app/models/models.py
+++ app/models/models.py
@@ -16,8 +16,8 @@ class User(Base):
     # 🔗 ORM Relationship to RefreshToken
     refresh_tokens = relationship(
         "RefreshToken",
-        back_populates="user",
-        cascade="all, delete-orphan"
+        back_populates = "user",
+        cascade = "all, delete-orphan"
     )
     
     def __repr__(self):
@@ -29,7 +29,8 @@ class RefreshToken(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     token = Column(String, unique=True, nullable=False)
-    user_id = Column(Integer, ForeignKey("user_login.id", ondelete="CASCADE"))  # ✅ fixed here
+    # So user_id is the foreign key that refers to the user_login table
+    user_id = Column(Integer, ForeignKey("user_login.id", ondelete="CASCADE"),unique = True)  
     expires_at = Column(DateTime, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
