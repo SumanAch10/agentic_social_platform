1ce515b72ccd03415878eb19698639fead4b7c9a
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-20 22:02:47 -0500

    Entry to the program

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 # from routes import routes
 from fastapi import FastAPI
 from app.routes import routes
+from app.models import models
+from app.db.db import Base,engine
 
 app = FastAPI()
+Base.metadata.create_all(bind = engine)
 
 app.include_router(routes.router)
 
