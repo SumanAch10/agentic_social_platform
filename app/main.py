16ab27be4c30bfa97ba6488bee1dc7d715122d78
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-21 15:50:32 -0500

    More routes added

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 # from routes import routes
 from fastapi import FastAPI
-from app.routes import routes
+from app.routes.auth_router import router
 from app.models import models
 from app.db.db import Base,engine
 
 app = FastAPI()
 Base.metadata.create_all(bind = engine)
 
-app.include_router(routes.router)
+app.include_router(router,prefix="/auth")
 
 
