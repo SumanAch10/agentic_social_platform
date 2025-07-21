eaee400960d580e13c0e0dc666ca04becf8099fe
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-21 15:49:13 -0500

    routing for authentication

---
+++ app/routes/auth_router.py
@@ -0,0 +1,34 @@
+from app.services import services
+from fastapi import APIRouter,Depends
+from app.schemas.users import UserCreate,UserLogin
+from app.utils.utils  import verify_access_token
+
+router = APIRouter()
+# print(type(router))
+
+@router.post("/user_create")
+def create_user_route(user: UserCreate):
+    return services.create_user(user)
+
+# Loggin in an user
+@router.post("/user_login")
+def login_user(user:UserLogin):
+    return  services.login_user(user)
+
+@router.get("/protected")
+def get_protected_route(current_user_email: str = Depends(verify_access_token)):
+    return {"message": f"Welcome {current_user_email}"}
+
+# @router.get("/{user_name}")
+# def get_byuser_name(user_name:str):
+#     return services.getUser(user_name)
+
+@router.get("/refresh")
+def refresh_token(response:dict  = Depends(services.renew_access_token)):
+    return response
+
+            
+    
+        
+        
+
