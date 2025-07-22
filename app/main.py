4e174020cfbe34e204654760443901e7a0bc1d50
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-21 22:31:11 -0500

    Entry point

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 # from routes import routes
 from fastapi import FastAPI
-from app.routes.auth_router import router
+from app.routes.auth_router import router as auth_router
+from app.routes.post_router import router as post_router
 from app.models import models
 from app.db.db import Base,engine
 
 app = FastAPI()
 Base.metadata.create_all(bind = engine)
 
-app.include_router(router,prefix="/auth")
+app.include_router(auth_router)
+app.include_router(post_router)
+
 
 
