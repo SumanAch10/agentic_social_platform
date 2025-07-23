be89ced9319b7d1ef052932b8fd2feecc775ddbd
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-22 22:11:04 -0500

    Utilities function for posts

--- app/utils/post_utils.py
+++ app/utils/post_utils.py
@@ -1,1 +1,13 @@
-# Utilities function fort post CRUD
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
+from fastapi import Depends,HTTPException,status
+from fastapi.security import OAuth2PasswordBearer
+from app.db.db import SessionLocal
+
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 5
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+def get_current_user(jwt_refresh_token:str)-> str:
+    print("Refresh_Token: ", jwt_refresh_token)
