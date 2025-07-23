a82d0a6394e94c4a711769c228ef39a20ffdc8f5
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-22 22:10:37 -0500

    Logic for services

--- app/services/post_services.py
+++ app/services/post_services.py
@@ -1,13 +1,16 @@
 # Business logic related to post
-from fastapi import HTTPException,status,Depends
+from fastapi import HTTPException,status,Depends,Cookie
 # from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from app.models.models import UserPost
-from app.schemas.users import UserPosts
+from app.schemas.users import UserPosts as pydantic_UserPost
 from app.db.db import SessionLocal
 from datetime import datetime
+from app.utils.post_utils import get_current_user
 
-# Function to create the post
-def create_post():
-    pass
-
+# Function to create the posts
+def create_post(post:pydantic_UserPost,jwt_refresh_token:str = Cookie(None)):
+    db:Session = SessionLocal()
+    # So i need to get the current user
+    print(jwt_refresh_token)
+    get_current_user(jwt_refresh_token)
