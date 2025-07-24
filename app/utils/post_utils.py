f7e67473bb52bc36e6781c205487d7efad39d0b9
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-23 19:21:52 -0500

    Utilities function for posts CRUD

--- app/utils/post_utils.py
+++ app/utils/post_utils.py
@@ -1,13 +1,26 @@
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
-from fastapi import Depends,HTTPException,status
+from fastapi import Depends,HTTPException,status,Cookie
 from fastapi.security import OAuth2PasswordBearer
 from app.db.db import SessionLocal
+from app.models.models import User
 
 SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 5
 REFRESH_TOKEN_EXPIRE_DAYS = 7
 
-def get_current_user(jwt_refresh_token:str)-> str:
-    print("Refresh_Token: ", jwt_refresh_token)
+def get_current_userId(jwt_refresh_token:str = Cookie(None))-> str:
+    db:Session = SessionLocal()
+    credentials_exception = HTTPException(
+        status_code = status.HTTP_401_UNAUTHORIZED,detail = "Invalid credentials")
+    try:
+        payload = jwt.decode(jwt_refresh_token,SECRET_KEY,algorithms=[ALGORITHM])
+        user_email = payload.get("sub")
+        db_user = db.query(User).filter(User.email == user_email).first()
+        return db_user.id
+        
+    finally:
+        # Preventing any leaks in case of error
+        db.close()
+    
