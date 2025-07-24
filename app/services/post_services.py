4d7b0832dec92cba37e7f36f6b74deaf3d3ae81e
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-23 19:21:05 -0500

    Logic for posts CRUD

--- app/services/post_services.py
+++ app/services/post_services.py
@@ -6,11 +6,17 @@ from app.models.models import UserPost
 from app.schemas.users import UserPosts as pydantic_UserPost
 from app.db.db import SessionLocal
 from datetime import datetime
-from app.utils.post_utils import get_current_user
+from app.utils.post_utils import get_current_userId
 
 # Function to create the posts
-def create_post(post:pydantic_UserPost,jwt_refresh_token:str = Cookie(None)):
-    db:Session = SessionLocal()
-    # So i need to get the current user
-    print(jwt_refresh_token)
-    get_current_user(jwt_refresh_token)
+def create_post(post:pydantic_UserPost,current_userId):
+    db: Session = SessionLocal()
+    # Establishing the connection between posts and user table
+    try:
+        user_post = UserPost(user_text = post.user_text,user_id = current_userId,image_url = post.image_url)
+        db.add(user_post)
+        db.commit()
+        db.refresh(user_post)
+        
+    finally:
+        db.close()
