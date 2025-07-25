c25ba60a77d58716c2e52d6352f8ea345cc2f944
SumanAch10 <contact.sumanacharya@gmail.com>
2025-07-24 21:03:34 -0500

    feat(auth): add utility to extract user ID from refresh token cookie

--- app/utils/post_utils.py
+++ app/utils/post_utils.py
@@ -10,6 +10,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 5
 REFRESH_TOKEN_EXPIRE_DAYS = 7
 
+# this function gives current user id
 def get_current_userId(jwt_refresh_token:str = Cookie(None))-> str:
     db:Session = SessionLocal()
     credentials_exception = HTTPException(
