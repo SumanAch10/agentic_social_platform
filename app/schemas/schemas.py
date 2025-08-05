8841e1b7591483d2fbea18f0ecad8f4f0b74c694
SumanAch10 <contact.sumanacharya@gmail.com>
2025-08-04 20:56:30 -0500

    feat:Schema for postresponse

---
+++ app/schemas/schemas.py
@@ -0,0 +1,48 @@
+# Defining the schemas for my users in this file
+from pydantic import BaseModel, EmailStr, field_validator, model_validator,constr
+from datetime import datetime
+
+from typing import Optional
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    confirm_password: str
+    user_name: str
+
+    @model_validator(mode = 'after')
+    def check_passwords_match(self):
+        if self.password != self.confirm_password:
+            raise ValueError("Passwords do not match")
+        return self
+    
+class UserLogin(BaseModel):
+    email:EmailStr
+    password:constr(min_length=6)
+
+# Pydantic models for the post
+class UserPosts(BaseModel):
+    user_text:Optional[str] = None
+    image_url:Optional[str] = None
+    
+    @model_validator(mode = "after")
+    def check_image_text(self):
+        if self.user_text is None and self.image_url is None:
+            raise ValueError("Either user_text or image should be provided")
+        return self
+        
+class PostResponse(BaseModel):
+    id:int
+    user_id : int
+    user_text : Optional[str] = None
+    image_url : Optional[str] = None
+    created_at : datetime
+    likes : int
+    comments : int
+    share : int
+    
+    class Config:
+        orm_mode = True
+    
+
